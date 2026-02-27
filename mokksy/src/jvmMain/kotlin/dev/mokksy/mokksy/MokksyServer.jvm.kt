b7981dbbbcd51529781dc37da462b94fea79faf9
kpavlov <1517853+kpavlov@users.noreply.github.com>
2026-02-27 15:46:13 +0200

    refactor: Update Mokksy server methods to support custom CoroutineDispatchers
    - Enhanced `start` and `shutdown` methods with optional `CoroutineDispatcher` parameter.
    - Documented dispatcher usage in function comments.

--- mokksy/src/jvmMain/kotlin/dev/mokksy/mokksy/MokksyServer.jvm.kt
+++ mokksy/src/jvmMain/kotlin/dev/mokksy/mokksy/MokksyServer.jvm.kt
@@ -7,6 +7,7 @@ import io.ktor.server.engine.EmbeddedServer
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import io.ktor.server.plugins.calllogging.CallLogging
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import org.slf4j.event.Level
@@ -28,18 +29,35 @@ internal actual fun createEmbeddedServer(
         }
     }
 
-public fun Mokksy.start() {
-    runBlocking(Dispatchers.Default) {
+/**
+ * Starts the Mokksy server, allowing it to process and respond to requests.
+ *
+ * @param dispatcher The CoroutineDispatcher to be used for blocking operations. Defaults to Dispatchers.Default.
+ */
+@JvmOverloads
+public fun Mokksy.start(dispatcher: CoroutineDispatcher = Dispatchers.Default) {
+    runBlocking(dispatcher) {
         this@start.startSuspend()
     }
 }
 
+/**
+ * Initiates the shutdown process of the Mokksy embedded server.
+ *
+ * @param gracePeriodMillis The duration in milliseconds for the server to attempt a graceful shutdown.
+ *        By default, it is set to 500 milliseconds.
+ * @param timeoutMillis The maximum duration in milliseconds to wait for the shutdown process to complete.
+ *        The default value is 1000 milliseconds.
+ * @param dispatcher The coroutine dispatcher on which the shutdown process will run. By default, it uses
+ *        `Dispatchers.Default`.
+ */
 @JvmOverloads
 public fun Mokksy.shutdown(
     gracePeriodMillis: Long = 500,
     timeoutMillis: Long = 1000,
+    dispatcher: CoroutineDispatcher = Dispatchers.Default,
 ) {
-    runBlocking(Dispatchers.Default) {
+    runBlocking(dispatcher) {
         this@shutdown.shutdownSuspend(gracePeriodMillis, timeoutMillis)
     }
 }
